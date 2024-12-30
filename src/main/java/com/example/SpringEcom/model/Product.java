74bbdf679097081d1a1b7f62eb64d53ecca1cf41
masira-syd29 <maseera.sayed29@gmail.com>
2024-12-30 13:02:09 +0530

    Initial Commit

--- src/main/java/com/example/SpringEcom/model/Product.java
+++ src/main/java/com/example/SpringEcom/model/Product.java
@@ -1,13 +1,12 @@
 package com.example.SpringEcom.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.xml.transform.sax.SAXTransformerFactory;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -16,14 +15,35 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
-    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
     private String brand;
     private BigDecimal price;
     private String category;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
     private boolean productAvailable;
     private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
+
+    public Product(int i) {
+    }
+    /* {
+  "name": "Colgate Toothpaste",
+  "description": "A fluoride toothpaste that helps fight cavities and freshens breath.",
+  "brand": "Colgate",
+  "price": 289,
+  "category": "Mobile",
+  "stockQuantity": 200,
+  "releaseDate": "2024-04-17",
+  "productAvailable": true
+}*/
+
+
 }
