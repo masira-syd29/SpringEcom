68c02d5140c69a6b8f4f3dea26ab116f8ffa49eb
masira-syd29 <maseera.sayed29@gmail.com>
2024-12-31 07:14:29 +0530

    Update ProductController.java

--- src/main/java/com/example/SpringEcom/controller/ProductController.java
+++ src/main/java/com/example/SpringEcom/controller/ProductController.java
@@ -31,6 +31,15 @@ public class ProductController {
         else
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+    @GetMapping("product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+        Product product = productService.getProductById(productId);
+
+        if(product.getId() > 0)
+            return new ResponseEntity<>(product.getImageData(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 
     @PostMapping("/product")
     public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile){
